69726a580850f169f0cb9b0777f8a55b0a06dc93
snowome <wjfbusy@163.com>
2022-08-14 23:56:11 +0800

    验证中间件

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,19 +1,17 @@
 const router = require('koa-router')()
-const { isExist, register } = require('../../controller/user.js')
+const {isExist, register} = require('../../controller/user.js')
 const userValidate = require('../../validator/user.js')
+const {genValidator} = require('../../middlewares/validator.js')
 
 router.prefix('/api/user')
 
-router.post('/register', async (ctx, next) => {
-    // 校验
-    userValidate()
-}, async (ctx, next) => {
-    const { userName, password, gender } = ctx.request.body
-    ctx.body = await register({ userName, password, gender })
+router.post('/register', genValidator(userValidate), async (ctx, next) => {
+    const {userName, password, gender} = ctx.request.body
+    ctx.body = await register({userName, password, gender})
 })
 
 router.post('/isExist', async (ctx, next) => {
-    const { userName } = ctx.request.body
+    const {userName} = ctx.request.body
     ctx.body = await isExist(userName)
 })
 
